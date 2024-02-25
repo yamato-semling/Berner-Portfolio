6ddf0872f204b75c87dae828e026607d10df6095
Yamato Semling <yamatosemling@gmail.com>
2024-02-25 22:54:24 +0100

    Docker

--- src/config.php
+++ src/config.php
@@ -10,16 +10,16 @@ $urlRoot = ((SSL) ? 'https' : 'http') . "://" .
     ((array_key_exists('SERVER_NAME', $_SERVER)) ? $_SERVER['SERVER_NAME'] : "localhost") .
     str_replace(str_replace("\\", "/", $documentRoot), "", str_replace("\\", "/", DIR_ROOT));
 
-define("URL_ROOT", $urlRoot . ((substr($urlRoot, -1) != "/") ? "/" : ""));
-define("DIR_IMG", 'http://localhost:8080/assets/img/');
-define("DIR_HAIKEI", 'http://localhost:8080/img/haikei/');
-define("DIR_ICON", 'http://localhost:8080/img/icon/');
-define("DIR_LOGOS", 'http://localhost:8080/assets/img/logos/');
-define("DIR_LOGOX", 'http://localhost:8080/assets/img/logo-ex/');
-define("DIR_PORTRAIT", 'http://localhost:8080/assets/img/portrait/');
-define("DIR_POINTS", 'http://localhost:8080/assets/img/points/');
-define("DIR_POSTER", 'http://localhost:8080/assets/img/poster/');
-define("DIR_TESTEMONIAL", 'http://localhost:8080/assets/img/testemonials/');
+define("URL_ROOT", "http://localhost:8080/");
+define("DIR_IMG", URL_ROOT . 'assets/img/');
+define("DIR_HAIKEI", URL_ROOT . 'assets/img/haikei/');
+define("DIR_ICON", URL_ROOT . 'assets/img/icon/');
+define("DIR_LOGOS", URL_ROOT . 'assets/img/logos/');
+define("DIR_LOGOX", URL_ROOT . 'assets/img/logo-ex/');
+define("DIR_PORTRAIT", URL_ROOT . 'assets/img/portrait/');
+define("DIR_POINTS", URL_ROOT . 'assets/img/points/');
+define("DIR_POSTER", URL_ROOT . 'assets/img/poster/');
+define("DIR_TESTEMONIAL", URL_ROOT . 'assets/img/testemonials/');
 define("HEADER", URL_ROOT . "header.php");
 
 function load_css(){
